2248c281c652a1693d08c5d5d10d59fe9a9c91f6
diabasecoin <diabasecoin@gmail.com>
2023-08-15 16:55:52 -0700

    Warning Removal
    Unknown block version being mined warning removed

--- test/functional/feature_versionbits_warning.py
+++ test/functional/feature_versionbits_warning.py
@@ -80,14 +80,13 @@ class VersionBitsWarningTest(BitcoinTestFramework):
         assert(not VB_PATTERN.match(node.getmininginfo()["warnings"]))
         assert(not VB_PATTERN.match(node.getnetworkinfo()["warnings"]))
 
-        self.log.info("Check that there is a warning if >50 blocks in the last 100 were an unknown version")
         # Build one period of blocks with VB_THRESHOLD blocks signaling some unknown bit
         self.send_blocks_with_version(node.p2p, VB_THRESHOLD, VB_UNKNOWN_VERSION)
         node.generate(VB_PERIOD - VB_THRESHOLD)
 
         # Check that get*info() shows the 51/100 unknown block version error.
-        assert(WARN_UNKNOWN_RULES_MINED in node.getmininginfo()["warnings"])
-        assert(WARN_UNKNOWN_RULES_MINED in node.getnetworkinfo()["warnings"])
+        #assert(WARN_UNKNOWN_RULES_MINED in node.getmininginfo()["warnings"])
+        #assert(WARN_UNKNOWN_RULES_MINED in node.getnetworkinfo()["warnings"])
 
         self.log.info("Check that there is a warning if previous VB_BLOCKS have >=VB_THRESHOLD blocks with unknown versionbits version.")
         # Mine a period worth of expected blocks so the generic block-version warning
