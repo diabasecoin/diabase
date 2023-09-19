e96f3ab45424af60cfcd715386cefb7b8f78b1c0
diabasecoin <diabasecoin@gmail.com>
2023-09-19 15:10:30 -0700

    Update Year
    Updated copyright year

--- src/bench/string_cast.cpp
+++ src/bench/string_cast.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2020 The Diabase Core developers
+// Copyright (c) 2023 The Diabase Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
